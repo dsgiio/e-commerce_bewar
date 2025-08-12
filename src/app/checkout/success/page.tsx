f0b38b278047bb1f65638f21bc9988e6d4443543
dsgiio <giosouzadantas@gmail.com>
2025-08-12 18:18:19 -0300

    feat: add checheckout success page

--- src/app/checkout/success/page.tsx
+++ src/app/checkout/success/page.tsx
@@ -1,9 +1,54 @@
-// src/app/checkout/success/page.tsx
-export default function CheckoutSuccessTest() {
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { Header } from "@/components/common/header";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+const CheckoutSuccessPage = () => {
   return (
-    <div style={{ padding: 40 }}>
-      <h1>Rota /checkout/success — OK</h1>
-      <p>Se você está vendo isso, a rota existe.</p>
-    </div>
+    <>
+      <Header />
+      <Dialog open={true} onOpenChange={() => {}}>
+        <DialogContent className="text-center">
+          <Image
+            src="/illustration.svg"
+            alt="Success"
+            width={300}
+            height={300}
+            className="mx-auto"
+          />
+          <DialogTitle className="mt-4 text-2xl">Pedido efetuado!</DialogTitle>
+          <DialogDescription className="font-medium">
+            Seu pedido foi efetuado com sucesso. Você pode acompanhar o status
+            na seção de “Meus Pedidos”.
+          </DialogDescription>
+
+          <DialogFooter>
+            <Button className="rounded-full" size="lg">
+              Ver meus pedidos
+            </Button>
+            <Button
+              className="rounded-full"
+              variant="outline"
+              size="lg"
+              asChild
+            >
+              <Link href="/">Voltar para a loja</Link>
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
-}
+};
+
+export default CheckoutSuccessPage;
